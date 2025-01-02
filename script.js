7dc49b0f9f178eef9b0bd8ee3e425b4ede43f049
Nath <it23834774@my.sliit.lk>
2025-01-02 11:10:54 +0530

    set footer

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch('./footer.html').then(response => response.text()).then(html => {
+    const temp = document.createElement("temp");
+    temp.innerHTML = html;
+    const footer = temp.querySelector("#footer");
+    document.getElementById("footer").innerHTML = footer.innerHTML;
+})
