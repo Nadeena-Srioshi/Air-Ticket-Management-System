a42ab30d233ce5ab44f8349f4ce1f4d61493cd79
Nath <it23834774@my.sliit.lk>
2025-02-16 19:30:50 +0530

    rate limit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("./db/connect"); //includes dotenv
+require("express-async-errors");
 const express = require("express");
 const session = require("express-session");
 const app = express();
@@ -8,13 +9,31 @@ const flights = require("./routes/flights");
 const bookings = require("./routes/bookings");
 const core = require("./routes/core");
 const notFound = require("./middleware/not-found");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 app.set("view engine", "ejs");
 
-//middleware
 app.use(express.static("./public"));
 
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
 app.use(express.json());
+
+// app.use(helmet());
+
+// app.use(cors());
+
+// app.use(xss());
 
 app.use(
   session({
@@ -35,7 +54,7 @@ app.use("/api/v1/bookings", bookings);
 
 app.use("/", core);
 
-//app.use(notFound);
+app.use(notFound);
 
 const port = 3000;
 
